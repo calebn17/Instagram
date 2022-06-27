21b89d8f2915270a810881b7af29505e2084c3cb
calebn17 <ccngai17@utexas.edu>
2022-06-27 13:30:29 -0500

    first commit

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Caleb Ngai on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
